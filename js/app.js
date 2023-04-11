813736da78885e1a87bc5f5f40eca65002140e71
Eugenia Cáceres <eugeniacaceres.mh@gmail.com>
2023-04-11 20:01:19 -0300

    arregle lógica de los alerts

--- js/app.js
+++ js/app.js
@@ -6,51 +6,34 @@ var boton_enviar_numero = document.getElementById("boton-enviar-num");
 
 function toogleFormulario() {
   var formulario = document.getElementById("formulario_juego");
-  formulario.style.visibility =
-    formulario.style.visibility === "hidden" ? "visible" : "hidden";
+  formulario.style.display =
+    formulario.style.display === "none" ? "block" : "none";
 
   numero_aleatorio = Math.floor(Math.random() * 100 + 1);
 }
+
+var exampleModal = document.getElementById("exampleModal");
+var modalTitle = exampleModal.querySelector(".modal-title");
+var modalBodyInput = exampleModal.querySelector(".modal-body");
 
 // BOTON ENVIAR NUMERO
 
 function enviar_numero() {
   boton_iniciar_juego.disabled = true;
+
   var numero = document.getElementById("input-numero").value;
 
   if (numero_aleatorio > numero) {
-    alert("¡Perdiste! El número mágico es más grande.");
+    modalTitle.textContent = "¡Perdiste!";
+    modalBodyInput.textContent = "El número mágico es más grande que " + numero;
+
+    // alert("¡Perdiste! El número mágico es más grande.");
   } else if (numero_aleatorio < numero) {
-    alert("¡Perdiste! El número mágico es más chico.");
+    modalTitle.textContent = "¡Perdiste!";
+    modalBodyInput.textContent = "El número mágico es más chico que " + numero;
+    // alert("¡Perdiste! El número mágico es más chico.");
   } else {
-    fireworks();
-    notificacion_juego("¡Ganaste!");
+    modalTitle.textContent = "¡Ganaste!";
+    modalBodyInput.textContent = "Felicidades!!";
   }
-}
-
-//////////////////////////////////////////
-
-function notificacion_juego(message) {
-  var alertBox = document.createElement("div");
-  alertBox.classList.add("my-alert-box");
-  alertBox.textContent = message;
-  document.body.appendChild(alertBox);
-}
-
-function fireworks() {
-  var fireworks = document.createElement("div");
-  fireworks.classList.add("fireworks");
-  document.body.appendChild(fireworks);
-
-  for (var i = 0; i < 20; i++) {
-    var spark = document.createElement("div");
-    spark.classList.add("spark");
-    spark.style.left = Math.random() * 100 + "%";
-    spark.style.top = Math.random() * 100 + "%";
-    fireworks.appendChild(spark);
-  }
-
-  setTimeout(function () {
-    fireworks.remove();
-  }, 1000);
 }
